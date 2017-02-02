9dd7c8b7883e5ca5b05e5a6facb665ef4c611db9
Xu Li <xuli0124@gmail.com>
2017-02-01 22:46:42 -0800

    node.js lab

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,32 @@
  */
 
 exports.view = function(req, res){
-  res.render('index');
+  res.render('index',{
+  	'projects':[
+  	{'name': 'waiting in Line',
+  	'image': 'lorempixel.people.1.jpeg',
+  	'id': 'project1'
+  	},
+  	{'name': 'Needfinding',
+  	'image': 'lorempixel.city.1.jpeg',
+  	'id': 'project2'
+  	},
+  	{'name': 'Prototype',
+  	'image': 'lorempixel.technics.1.jpeg',
+  	'id': 'project3'
+  	},
+  	{'name': 'Heuristic Evaluation',
+  	'image': 'lorempixel.abstract.1.jpeg',
+  	'id': 'project4'
+  	},
+  	{'name': 'Visualization',
+  	'image': 'lorempixel.people.2.jpeg',
+  	'id': 'project5'
+  	},
+  	{'name': 'Social Design',
+  	'image': 'lorempixel.technics.2.jpeg',
+  	'id': 'project6'
+  	},
+  	]
+  });
 };
